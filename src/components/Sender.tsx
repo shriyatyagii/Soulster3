25029b47ea22395e1db92b182a646897e872eb1b
PacSamil <parv21.chaudhary@gmail.com>
2021-09-03 15:41:38 +0530

    UI Update

--- src/components/Sender.tsx
+++ src/components/Sender.tsx
@@ -85,22 +85,32 @@ const onClickSendMoney = async (
 
 
 return (
-  <div>
-  <form className="wrapper">
-  <div id="wizard">
-    <label>Amount: </label>
-    <div className="form-row"><input type="text" onChange={onChangeAmount} value={amount} className="form-control" ></input></div>
-    <label>Address: </label>  
-    <div className="form-row"><input type="text" onChange={onChangeAddress} value={address} className="form-control"></input></div>
-    <label>Time: </label>
-    <div className="form-row"><input type="text" onChange={onChangeTimeA} value={timeA} className="form-control"></input></div>
-    <label>Interval: </label>
-    <div className="form-row"><input type="range" min="1" max="100" onChange={onChangeIntervals} value={intervals} className="form-control"></input></div>
-    <label></label>
-    <div><button  onClick={onClickSendMoney}  className="button"><span>Create Stream</span></button></div>  
-    <div><button  onClick={onClickStop}  className="button"><span>Stop Stream</span></button></div>  </div>
-  </form>
-  </div>
+  <div className="main">
+      <div className="text" >
+        <div className="text-sub">
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+      </div>
+      </div>
+      <div className="rating">
+    <div className="rating-sub">
+    <div><button onClick={onClickStop} className="button-pr"><span>Stop Stream</span></button></div>
+    <label>Performance Rating: </label><label className="label">{intervals}</label>
+    <div className="pr"><input type="range" min="1" max="100" onChange={onChangeIntervals} value={intervals} className="slider"></input></div>
+    </div>
+    </div>
+    <div className="form">
+      <div className="form-sub">
+      <form className="wrapper">
+      <label>Amount: </label>
+      <div className="form-row"><input type="text" onChange={onChangeAmount} value={amount} placeholder="Amount" className="form-control" ></input></div>
+      <label>Address: </label>  
+      <div className="form-row"><input type="text" onChange={onChangeAddress} value={address} className="form-control"></input></div>
+      <label>Time: </label>
+      <div className="form-row"><input type="text" onChange={onChangeTimeA} value={timeA} className="form-control"></input></div>
+      <label>Interval: </label><label className="label">{intervals}</label>
+      <div className="form-row"><input type="range" min="1" max="100" onChange={onChangeIntervals} value={intervals} className="slider"></input></div>
+      <label></label>
+      <div><button  onClick={onClickSendMoney} className="button"><span>Create Stream</span></button></div></form> </div></div></div>
 );
 };
 
